45ba5b533dac59bb5ce390894d0021222e4d1475
killersmath <maxsuel.filomeno@unemat.br>
2020-02-27 02:02:36 -0400

    fix: changing the value of max request rate limit to only accept 10 requisitions per minute

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.use(cors(origin));
 //--- limiter (anti ddos)
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 150 // 5 requests,
+  max: 10 // 10 requests,
 });
 app.use(limiter);
 
